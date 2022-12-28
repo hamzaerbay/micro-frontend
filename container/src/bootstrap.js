12fe9ecfdf61842231c71da750dc585717c47d97
Hamza Erbay <hamzaerbay@gmail.com>
2022-12-27 20:44:06 -0800

    refactor: render product app in container
    HEADS UP:
    We’re importing the mount function from the product app. Currently, index.js is importing bootstrap.js and not exporting the mount function. Therefore we update the product/webpack.config.js
    ProductIndex was pointing to the index file, which is not exporting the mount function

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,6 +1,8 @@
-import "products/ProductsIndex";
+import { mount } from "products/ProductsIndex";
 import "cart/CartDetail";
 
+
+mount(document.querySelector('#render-products'))
 console.log("HOST: container");
 console.log("Remote: products");
-console.log("Remote: cart")
+console.log("Remote: cart");
--- products/webpack.config.js
+++ products/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
       name: "products",
       filename: "remoteEntry.js",
       exposes: {
-        "./ProductsIndex": "./src/index",
+        "./ProductsIndex": "./src/bootstrap",
       },
       shared: ["faker"],
     }),
