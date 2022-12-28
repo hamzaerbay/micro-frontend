c6ad307dd6e1a225f2fc027daab96da6f801b521
Hamza Erbay <hamzaerbay@gmail.com>
2022-12-27 20:34:10 -0800

    refactor: isolate product app
    Dev env uses isolated dom, and the container app can decide will decide to render the placement of the product app

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,11 +1,39 @@
 import faker from "faker";
 
-let products = "";
+const mount = (el) => {
+  let products = "";
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+  products += '<strong>Generated with "faker": "^5.1.0" </strong>';
+
+  el.innerHTML = products;
+};
+
+/*
+  Context #1
+  We're running this file in development in isolation
+  We're using local index.html file
+  Which definetly has an element with an id of "dev-product"
+  we want to immediately render our app into that element
+*/
+if (process.env.NODE_ENV === "development") {
+  /* 
+    webpack.config =>   mode: "development",
+    Purpose of #isolated-dev-products is only used by the product app itself, not in the outer apps
+  */
+  const el = document.querySelector("#isolated-dev-products");
+  if (el) {
+    mount(el);
+  }
 }
-products += '<strong>Generated with "faker": "^5.1.0" </strong>';
 
-document.querySelector("#dev-products").innerHTML = products;
+/*
+   Context #2
+   We're running this file in dev or prod through the container app
+   No guarantee that an element with an id of "dev-product"
+   we dont want to try immediately render app
+*/
+export { mount };
