77cab0f6ca27a5f482f6689441a5f2d9c746fa42
Hamza Erbay <hamzaerbay@gmail.com>
2022-12-27 20:08:59 -0800

    chore: update  dynamic import error comment

--- products/src/index.js
+++ products/src/index.js
@@ -1,4 +1,5 @@
 /* https://webpack.js.org/api/module-methods/#import-1
+  https://webpack.js.org/concepts/module-federation/#troubleshooting
   import(), Dynamically load modules that solve the load asynchronous js
   package error caused by faker.js (shared by module federation). 
   */
